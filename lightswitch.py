31919bb3f0b4c80c7b88342b22875ae39e2c8928
DBosDavinci <99069008@mydavinci.nl>
2021-11-03 10:02:53 +0100

    lightswitch updated

--- lightswitch.py
+++ lightswitch.py
@@ -6,7 +6,24 @@ button.pack(pady = 20, padx = 20)
 
 # schijf hier tussen je code
 
+flip = False
 
+def switch():
+    global flip,button
+    if flip == False:
+        button.config(text='button is on')
+        window.configure(bg='yellow')
+        print('button is on')
+        flip = True
+    else:
+        button.config(text='button is off')
+        window.configure(bg='black')
+        print('button is off')
+        flip = False
+
+window.configure(bg='black')
+button.config(text='button is off',command=switch)
+button.pack(pady = 20, padx = 20)
 
 # schijf hier tussen je code
 
